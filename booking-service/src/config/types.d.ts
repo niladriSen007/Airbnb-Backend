54dea67aad7562480f06fb6d03ef7a01482eb801
Niladri <niladrisengit12@gmail.com>
2025-08-03 19:22:42 +0530

    added redlock mechanism

--- booking-service/src/config/types.d.ts
+++ booking-service/src/config/types.d.ts
@@ -1,3 +1,5 @@
 export type ServerConfig = {
+  REDIS_SERVER_URL: string
   PORT: number,
+  LOCK_TTL: number
 }
--- booking-service/src/services/booking.services.ts
+++ booking-service/src/services/booking.services.ts
@@ -1,9 +1,11 @@
 import { CreateBookingDTO } from "../dto/booking.dto";
 import { BookingRepository } from "../repositories/booking/booking.repository";
 import { IdempotencyRepository } from "../repositories/idempotency/idempotency.repository";
-import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
+import { BadRequestError, InternalServerError, NotFoundError } from "../utils/errors/app.error";
 import { generateIdempotencyKey } from "../utils/idempotecyGeneration";
 import prismaClient from "../prisma/client";
+import { redlock } from "../config/redis.config";
+import { serverConfig } from "../config";
 
 export class BookingService {
   constructor(
@@ -12,19 +14,33 @@ export class BookingService {
   ) { }
 
   async createBooking({ userId, hotelId, bookingAmount, totalGuests }: CreateBookingDTO) {
-    const booking = await this.bookingRepository.createBooking({
-      userId,
-      hotelId,
-      bookingAmount,
-      totalGuests
-    })
 
-    const idempotencyKey = generateIdempotencyKey()
 
-    await this.idempotencyRepository.createIdempotencyKey(booking?.id, idempotencyKey)
-    return {
-      bookingId: booking?.id,
-      idempotencyKey
+    const time_to_live = serverConfig?.LOCK_TTL;  // for the next 5 minutes the hotel will be locked for us so no other user can book the same hotel
+    //but after 5 minutes the hotel will be available for booking again
+
+    const bookingLockResource = `hotel-${hotelId}`
+
+
+    try {
+      await redlock.acquire([bookingLockResource], time_to_live);
+
+      const booking = await this.bookingRepository.createBooking({
+        userId,
+        hotelId,
+        bookingAmount,
+        totalGuests
+      })
+
+      const idempotencyKey = generateIdempotencyKey()
+
+      await this.idempotencyRepository.createIdempotencyKey(booking?.id, idempotencyKey)
+      return {
+        bookingId: booking?.id,
+        idempotencyKey
+      }
+    } catch (error) {
+      throw new InternalServerError('Failed to acquire lock for booking resource');
     }
   }
 
@@ -32,7 +48,7 @@ export class BookingService {
   async finalizeBooking(idempotencyKey: string) {
 
     return await prismaClient.$transaction(async (tx) => {
-      const idempotency = await this.idempotencyRepository.getIdempotencyKeyWithLock(tx,idempotencyKey)
+      const idempotency = await this.idempotencyRepository.getIdempotencyKeyWithLock(tx, idempotencyKey)
 
       if (!idempotency || !idempotency?.bookingId) {
         throw new NotFoundError("Invalid idempotency key")
@@ -42,8 +58,8 @@ export class BookingService {
         throw new BadRequestError("Idempotency key is already finalized")
       }
 
-      const booking = await this.bookingRepository?.confirmBooking(tx,idempotency?.bookingId,)
-      await this.idempotencyRepository.finalizeIdempotencyKey(tx,idempotencyKey)
+      const booking = await this.bookingRepository?.confirmBooking(tx, idempotency?.bookingId,)
+      await this.idempotencyRepository.finalizeIdempotencyKey(tx, idempotencyKey)
       return booking
     })
 
