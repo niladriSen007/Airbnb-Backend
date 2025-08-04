2d105082f675cc7b8b131d8a0a0365916fe305e7
Niladri <niladrisengit12@gmail.com>
2025-08-04 07:33:49 +0530

    basic notofication service setup

---
+++ notification-service/src/config/index.ts
@@ -0,0 +1,19 @@
+
+import dotenv from 'dotenv';
+import { ServerConfig } from './types';
+
+
+function loadDotEnv() {
+  const env = dotenv.config();
+  if (env.error) {
+    throw env.error;
+  }
+  return "Environment variables loaded successfully";
+}
+loadDotEnv();
+
+export const serverConfig: ServerConfig = {
+  PORT: Number(process.env.PORT) || 4000,
+  REDIS_SERVER_URL: process.env.REDIS_SERVER_URL || "redis://localhost:6379",
+  LOCK_TTL: Number(process.env.LOCK_TTL) || 60000
+}
